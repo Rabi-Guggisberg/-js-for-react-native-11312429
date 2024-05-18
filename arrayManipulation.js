e4c95e51f734b0ddc3fb9f0a6a67ec2b51925769
Rabi-Guggisberg <jgmpiani@st.ug.edu.gh>
2024-05-18 20:31:05 +0100

    done with task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -12,3 +12,29 @@ function processArray(numbers) {
 }
 
 console.log(processArray([2,5,3,1,6,7,8]));
+
+
+function formatArrayStrings(stringArray, numbers) {
+    let str = []
+
+    for (let i = 0; i < stringArray.length; i++) {
+        let arrStr = stringArray[i]
+        let arrNum = numbers[i]
+
+        if (arrNum%2 === 0) {
+            str[str.length] = arrStr.toUpperCase()
+        } else {
+            str[str.length] = arrStr.toLowerCase()
+        }
+    }
+
+    return str
+}
+
+const a = formatArrayStrings(["a","b","c","d"], processArray([1,2,3,4]))
+console.log(a)
+
+module.exports = {
+    processArray,
+    formatArrayStrings
+}
